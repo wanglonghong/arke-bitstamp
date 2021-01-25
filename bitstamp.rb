a74784540c1aa3c8707f778e007e0f79c3ff469c
wanglonghong <wanglonghong4467@gmail.com>
2021-01-25 21:37:24 +0800

    Limit amount per order and cut off by spread.

--- bitstamp.rb
+++ bitstamp.rb
@@ -16,6 +16,8 @@ module Arke::Exchange
         builder.adapter :em_synchrony
       end
 
+      @conn2 = Faraday.new('https://www.bitstamp.net')
+
       @orderbook = Arke::Orderbook.new(@market)
       @last_update_id = 0
       @rest_api_connection = Faraday.new("https://#{opts['host']}") do |builder|
@@ -23,13 +25,22 @@ module Arke::Exchange
         builder.headers['Content-Type'] = 'application/x-www-form-urlencoded'
       end
 
-      @ws_countdown = 3
+      @limit_order_count = 30
       @origin_depth = opts['depth']
+      @max_amount_per_order = opts['max_amount_per_order'] || 0
+      @spread = opts['spread'] || 0.05
     end
 
     # TODO: remove EM (was used only for debug)
     def start
       get_snapshot
+
+      Thread.new do
+        loop do
+          sleep 60
+          get_snapshot_2
+        end
+      end
 
       EM.run do
         @ws = Faye::WebSocket::Client.new(@url)
@@ -40,7 +51,6 @@ module Arke::Exchange
               event: "bts:subscribe",
               data: {
                   channel: "diff_order_book_#{@market.downcase}"
-                  #channel: "order_book_#{@market.downcase}"
               },
           }
 
@@ -80,34 +90,57 @@ module Arke::Exchange
 
       @last_update_id = data['microtimestamp'].to_i
 
-      Arke::Log.info "Process Bitstamp order Bids: #{data['bids'].length} Asks: #{data['asks'].length}"
+      # Arke::Log.info "Process Bitstamp order Bids: #{data['bids'].length} Asks: #{data['asks'].length}"
 
       process(data['bids'], :buy) unless data['bids'].empty?
       process(data['asks'], :sell) unless data['asks'].empty?
     end
 
-    def process(data, side)
+    def process(data, side, from_snapshot = false)
+      best_price = data.first[0].to_f
+
       data.each do |order|
         if order[1].to_f < 10 ** (-8)
           @orderbook.delete(build_order(order, side))
           next
         end
 
+        if from_snapshot && !contains_in_spread(side, best_price, order[0].to_f)
+          @orderbook.delete(build_order(order, side))
+          next
+        end
+
+        if order[0].to_f.zero?
+          next
+        end
+
         orderbook.update(
           build_order(order, side)
         )
-
-        if orderbook.book[side].size > 100
-          orderbook.book[side].delete orderbook.book[side].last[0]
-        end
       end
+
+      if from_snapshot && @orderbook.book[side].size > 0
+        @orderbook.book[side].reject! {|price, amount| !contains_in_spread(side, best_price, price) }
+      end
+
+      
+      # if orderbook.book[side].size > @limit_order_count
+      #   total = orderbook.book[side].size
+      #   i = @limit_order_count
+      #   while i < total
+      #     orderbook.book[side].delete orderbook.book[side].last[0]
+      #     i += 1
+      #   end
+      #
+      #   Arke::Log.info " <======= Process Bitstamp #{side} last order Delete : #{total} -  #{i - @limit_order_count} "
+      # end
     end
 
     def build_order(data, side)
       Arke::Order.new(
         @market,
         data[0].to_f,
-        data[1].to_f,
+        [data[1].to_f, @max_amount_per_order].min,
         side
       )
     end
@@ -115,8 +148,27 @@ module Arke::Exchange
     def get_snapshot
       snapshot = JSON.parse(@connection.get("api/v2/order_book/#{@market.downcase}").body)
       @last_update_id = snapshot['microtimestamp'].to_i
-      process(snapshot['bids'], :buy)
-      process(snapshot['asks'], :sell)
+      process(snapshot['bids'][0...@limit_order_count], :buy, true)
+      process(snapshot['asks'][0...@limit_order_count], :sell, true)
+    end
+
+    def get_snapshot_2
+      snapshot = JSON.parse(@conn2.get("api/v2/order_book/#{@market.downcase}").body)
+      @last_update_id = snapshot['microtimestamp'].to_i
+
+      Arke::Log.info "Process Snapshot Bids: #{snapshot['bids'].length} Asks: #{snapshot['asks'].length}"
+
+      process(snapshot['bids'][0...10], :buy, true)
+      process(snapshot['asks'][0...10], :sell, true)
+    end
+
+    def contains_in_spread(side, best_price, price)
+      if best_price <= 0.0
+        true
+      else
+        return  best_price * (1 - @spread) <= price && price <= best_price if side === :buy
+        return  best_price <= price && price <= best_price * (1 + @spread) if side === :sell
+      end
     end
 
     def create_order(order)
